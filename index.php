233b43df102bd1e426518db4c67741072ced7247
Briggsby <jackbriggsby@gmail.com>
2018-10-27 22:57:50 +0100

    Revert "Merge branch 'master' of https://github.com/glasgow18/team-9"
    This reverts commit 9d80f7989458a72aadeded90a3a5080af27f9195, reversing
    changes made to 357f551a0571e09cf4476947ccdc9cb7ce9970ce.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include ('header.php');
+?>
+<!-- Search form -->
+<div class="md-form mt-0">
+    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+</div>
+
+<?php
+include ('footer.php');
+?>
