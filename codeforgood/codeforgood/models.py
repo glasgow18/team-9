357f551a0571e09cf4476947ccdc9cb7ce9970ce
Briggsby <jackbriggsby@gmail.com>
2018-10-27 22:43:47 +0100

    do you pronounce it data or data (fixed some database stuff)

--- codeforgood/codeforgood/models.py
+++ codeforgood/codeforgood/models.py
@@ -22,22 +22,30 @@ class Locations(models.Model):
     website = models.CharField(max_length=255)
     cost = models.CharField(max_length=255)
     description = models.CharField(max_length=4095)
-    tags = models.CharField(max_length=4095)
-    accessibility = models.CharField(max_length=4095)
     date = models.DateField()
-    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL, blank=True, null=True, default=None)
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL, blank=True, null=True, default=None)
 
     def __str__(self):
         return self.name
 
 
+class Tags(models.Model):
+    tag = models.CharField(max_length=255)
+    accessibility = models.BooleanField(default = False)
+
+
+class Location_Tags(models.Model):
+    location = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Locations, on_delete=models.CASCADE)
+
+
 class Favourites(models.Model):
-    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, default=None)
-    favourite_loc_id = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True, default=None)
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, default=None)
+    favourite_loc = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True, default=None)
 
 
 class Saved_Searches(models.Model):
-    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, default=None)
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, default=None)
     location = models.CharField(max_length=255)
     search = models.CharField(max_length=255)
 
@@ -46,16 +54,17 @@ search_filters = models.CharField(max_length=255)
 
 
 class Suggestions(models.Model):
-    loc_id = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True, default=None)
-    user_id = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, default=None)
+    loc = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True, default=None)
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL, null=True, default=None)
     suggestion = models.CharField(max_length=6000)
 
 
 class Warnings(models.Model):
-    loc_id = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True, default=None)
+    loc = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True, default=None)
     warning = models.CharField(max_length=255)
 
 
 class Categories(models.Model):
     category = models.CharField(max_length=255)
     subcategory = models.CharField(max_length=255)
+
