dcba07a5d16e7cfc474a77ae73b2a9bd2ea186a2
Briggsby <jackbriggsby@gmail.com>
2018-10-28 00:05:53 +0100

    Dumb dumb search

--- codeforgood/codeforgood/views.py
+++ codeforgood/codeforgood/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from models import Locations
 from django.http import HttpResponse
 import datetime
 
 def index(request):
     return render(request, 'index.html')
+
+
+def search_dummy(request):
+    if request.method == 'GET':
+        status = Locations.objects.filter(id < 5)
+        return render(request,"search.html", {"locations":status})
