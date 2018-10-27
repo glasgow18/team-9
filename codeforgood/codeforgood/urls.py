145f5bad1b9a257a7d48c56805d6f2382bb27f10
juurailmjarv <juura.ilmjarv@gmail.com>
2018-10-27 23:24:59 +0100

    Website is working

--- codeforgood/codeforgood/urls.py
+++ codeforgood/codeforgood/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from codeforgood import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
 ]
+
+
